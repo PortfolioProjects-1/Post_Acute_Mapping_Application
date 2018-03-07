80ba273c0ca729fa8eacd8d68ca372a2b0a13203
khwilson5692 <32885907+khwilson5692@users.noreply.github.com>
2018-03-06 21:19:05 -0600

    Add files via upload

--- Source_Code/ui_server.R
+++ Source_Code/ui_server.R
@@ -5,45 +5,55 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-#
 library(shinydashboard)
 library(leaflet)
 library(data.table)
-library(DT)
+library(DT, lib.loc = "C:/Program Files/R/R-3.4.3/library")
+library(htmltools)
 # Define body of dashboard
 ui <- dashboardBody(
 fluidRow(
   column(width = 9,
          box(width = NULL, solidHeader = TRUE,
-             leafletOutput("SNF_Provider_Map", height = 500)
+             leafletOutput("SNF_Provider_Map", width = "100%", height = 1000)
          ),
-         
-         box(width = NULL,
-             #uiOutput("PacProviderTable"),
-            DT::dataTableOutput("PacProviderTable")
-         )),
-         column(width = 3,
-          box(width = NULL, status = "warning",
-            selectInput("year", "Year:", c("2013", "2014", "2015")),
-                  selectInput("radius", "Radius:",c("5","10","15","20")),
-                    selectInput("condition", "Condition:",c("Asthma","Diabetes","Hyperlipidemia"))))))
+      
+  
+  absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
+                draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
+                width = 330, height = "auto",
+                selectInput("year", "Year:", c("2013", "2014", "2015")),
+                       selectInput("radius", "Radius:",c("5","10","15","20")),
+                          selectInput("condition", "Condition:",c("Asthma","Diabetes","Hyperlipidemia")
+                
+                )))))
+         # column(width = 3,
+         #  box(width = NULL, status = "warning",
+         #    selectInput("year", "Year:", c("2013", "2014", "2015")),
+         #          selectInput("radius", "Radius:",c("5","10","15","20")),
+         #            selectInput("condition", "Condition:",c("Asthma","Diabetes","Hyperlipidemia"))))))
 
 
 # Define server logic 
 server <- function(input, output) {
 content<- "A"
-    output$SNF_Provider_Map <- renderLeaflet({
-    leaflet(SNF_2015_TX) %>% addPopups(SNF_2015_TX$lat, SNF_2015_TX$long, content,
-                                       options = popupOptions(closeButton = FALSE)) %>%
-                                         addTiles() %>% addMarkers(lat = ~long, lng = ~lat)})
+    # output$SNF_Provider_Map <- renderLeaflet({
+    # leaflet(SNF_2015_TX) %>% addPopups(SNF_2015_TX$lat, SNF_2015_TX$long, content,
+    #                                    options = popupOptions(closeButton = FALSE)) %>%
+    #                                      addTiles() %>% addMarkers(lat = ~long, lng = ~lat)})
     
      output$SNF_Provider_Map <- renderLeaflet({
-       leaflet(SNF_2015_TX) %>% 
+       leaflet(SNF_2015_TX) %>% ##change to filtered() 
          addTiles() %>%
            addCircleMarkers(lng = SNF_2015_TX$lat,lat = SNF_2015_TX$long, 
                                         
-                                          popup = ~Cost_Per_Day
-                                         ) 
+                                          popup = paste("<b>","Facility:","</b>", "<i>",SNF_2015_TX$Facility_Name,"</i>", "<br>",
+                                                        "<b>","Daily Cost:","</b>", as.character(round(SNF_2015_TX$Cost_Per_Day,2)), "<br>",
+                                                        "<b>","Avg Length Of Stay:","</b>", as.character(SNF_2015_TX$ALOS_Days)))
+                                                        #paste("Facility:", SNF_2015_TX$Facility_Name,"<br>",
+                                          #   "Daily Cost:",as.character(round(SNF_2015_TX$Cost_Per_Day,2),"<br>",
+                                          #                              "Daily Cost:",as.character(round(SNF_2015_TX$Cost_Per_Day,2))))
+                                         
         })
       
   
@@ -55,4 +65,19 @@ content<- "A"
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
+#`library(DT, lib.loc = "C:/Users/kenne/Documents/R/win-library/3.4")` I think will do it.
 
+
+
+filtered <- reactive ({
+  dataset %>%
+    filter(column1 == input$year, ##use id for input
+           column2 == input$radius, ##use id for input
+           column3 == input$condition)
+  
+})
+
+
+##Use filtered()
+
+##maybe use isolate to fix dropdown feature 
