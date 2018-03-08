c11fcdd70bdd84a267de3ea729b34eea2dfd20a0
khwilson5692 <32885907+khwilson5692@users.noreply.github.com>
2018-03-08 11:39:12 -0600

    Add files via upload

--- Source_Code/ui_server.R
+++ Source_Code/ui_server.R
@@ -1,15 +1,35 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
+
+
 library(shinydashboard)
 library(leaflet)
 library(data.table)
 library(DT, lib.loc = "C:/Program Files/R/R-3.4.3/library")
 library(htmltools)
+
+# API key
+# API_key <- "AIzaSyAPevncZzP2DndypLMvk66xZVsVpcN94c8"
+# 
+# Get_lat_long <- function(Address,API_key) {
+#   URL <- paste("https://maps.googleapis.com/maps/api/geocode/xml?address=",gsub(" ","+",Address),"&key=",API_key,sep="")
+#   webpage <- getURL(URL)
+#   XML_code <- xmlParse(webpage)
+#   XML_data <- xmlToList(XML_code)
+#   Result <- tryCatch(data.frame(
+#     Address = Address,
+#     lat = XML_data$result$geometry$location$lat,
+#     lng = XML_data$result$geometry$location$lng
+#   ),error = function(e) NA)
+#   Result$Address <- tryCatch(as.character(Result$Address),error = function(e) NA)
+#   Result$lat <- tryCatch(as.numeric(as.character(Result$lat)),error = function(e) NA)
+#   Result$lng <- tryCatch(as.numeric(as.character(Result$lng)),error = function(e) NA)
+#   return(tryCatch(Result,error = function(e) NA))
+# }
+
+# City list 
+City_list <- as.list(unique(SNF_2015_TX$City))
+names(City_list) <- unique(SNF_2015_TX$City)
+
+
 # Define body of dashboard
 ui <- dashboardBody(
 fluidRow(
@@ -21,40 +41,36 @@ fluidRow(
   
   absolutePanel(id = "controls", class = "panel panel-default", fixed = TRUE,
                 draggable = TRUE, top = 60, left = "auto", right = 20, bottom = "auto",
-                width = 330, height = "auto",
-                selectInput("year", "Year:", c("2013", "2014", "2015")),
+                width = 330, height = 1000,
+                selectInput("city", "City:",City_list),
                        selectInput("radius", "Radius:",c("5","10","15","20")),
                           selectInput("condition", "Condition:",c("Asthma","Diabetes","Hyperlipidemia")
+                                      
+
                 
                 )))))
-         # column(width = 3,
-         #  box(width = NULL, status = "warning",
-         #    selectInput("year", "Year:", c("2013", "2014", "2015")),
-         #          selectInput("radius", "Radius:",c("5","10","15","20")),
-         #            selectInput("condition", "Condition:",c("Asthma","Diabetes","Hyperlipidemia"))))))
-
-
+   
 # Define server logic 
 server <- function(input, output) {
-content<- "A"
-    # output$SNF_Provider_Map <- renderLeaflet({
-    # leaflet(SNF_2015_TX) %>% addPopups(SNF_2015_TX$lat, SNF_2015_TX$long, content,
-    #                                    options = popupOptions(closeButton = FALSE)) %>%
-    #                                      addTiles() %>% addMarkers(lat = ~long, lng = ~lat)})
+
+    plot_data <- reactive({
+      newData <- SNF_2015_TX
+      newData <- newData[(newData$City %in% input$city),]
+      
+    })
     
      output$SNF_Provider_Map <- renderLeaflet({
-       leaflet(SNF_2015_TX) %>% ##change to filtered() 
+       plot_data <- plot_data()
+       leaflet(plot_data) %>% ##change to filtered() 
          addTiles() %>%
-           addCircleMarkers(lng = SNF_2015_TX$lat,lat = SNF_2015_TX$long, 
+           addCircleMarkers(lng = plot_data$lat,lat = plot_data$long, 
                                         
-                                          popup = paste("<b>","Facility:","</b>", "<i>",SNF_2015_TX$Facility_Name,"</i>", "<br>",
-                                                        "<b>","Daily Cost:","</b>", as.character(round(SNF_2015_TX$Cost_Per_Day,2)), "<br>",
-                                                        "<b>","Avg Length Of Stay:","</b>", as.character(SNF_2015_TX$ALOS_Days)))
-                                                        #paste("Facility:", SNF_2015_TX$Facility_Name,"<br>",
-                                          #   "Daily Cost:",as.character(round(SNF_2015_TX$Cost_Per_Day,2),"<br>",
-                                          #                              "Daily Cost:",as.character(round(SNF_2015_TX$Cost_Per_Day,2))))
-                                         
-        })
+                                          popup = paste("<b>","Facility:","</b>", "<i>",plot_data$Facility_Name,"</i>", "<br>",
+                                                        "<b>","Daily Cost:","</b>", "$",as.character(round(plot_data$Cost_Per_Day,2)), "<br>",
+                                                        "<b>","Avg Length Of Stay:","</b>", as.character(plot_data$ALOS_Days), "<br>",
+                                                        "<b>", "TotalStays", input$condition,":","</b>", if (input$condition == "Asthma") {ceiling(plot_data$Total_Stays_Asthma)} 
+                                                        else if (input$condition == "Diabetes") {ceiling(plot_data$Total_Stays_Diabetes)} else {ceiling(plot_data$Total_Stays_Hyperlipidemia)}))
+                  })
       
   
     #output$PacProviderTable <- renderTable({DT::datatable(data = SNF_2015_TX,
@@ -65,17 +81,16 @@ content<- "A"
 }
 # Run the application 
 shinyApp(ui = ui, server = server)
-#`library(DT, lib.loc = "C:/Users/kenne/Documents/R/win-library/3.4")` I think will do it.
 
 
 
-filtered <- reactive ({
-  dataset %>%
-    filter(column1 == input$year, ##use id for input
-           column2 == input$radius, ##use id for input
-           column3 == input$condition)
-  
-})
+# filtered <- reactive ({
+#   dataset %>%
+#     filter(column1 == input$year, ##use id for input
+#            column2 == input$radius, ##use id for input
+#            column3 == input$condition)
+#   
+# })
 
 
 ##Use filtered()
